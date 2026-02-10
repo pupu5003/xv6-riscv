aaa8d3dd71e48da48dd18894b6031c524005adf7
pupu5003 <pnphuonguyen3005@gmail.com>
2026-02-10 11:00:55 +0700

    feat: Update wc.c

--- user/wc.c
+++ user/wc.c
@@ -5,14 +5,16 @@
 
 char buf[512];
 
+// option flag 
+int flag_l = 0, flag_w = 0, flag_c = 0;
+
 void
 wc(int fd, char *name)
 {
   int i, n;
-  int l, w, c, inword;
+  int l = 0, w = 0, c = 0;
+  int inword = 0;
 
-  l = w = c = 0;
-  inword = 0;
   while((n = read(fd, buf, sizeof(buf))) > 0){
     for(i=0; i<n; i++){
       c++;
@@ -30,7 +32,19 @@ wc(int fd, char *name)
     printf("wc: read error\n");
     exit(1);
   }
-  printf("%d %d %d %s\n", l, w, c, name);
+  
+  if(flag_l)
+    printf("%d ", l);
+  if(flag_w)
+    printf("%d ", w);
+  if(flag_c)
+    printf("%d ", c);
+
+  if(name && name[0])
+    printf("%s", name);
+
+  printf("\n");
+
 }
 
 int
@@ -38,12 +52,35 @@ main(int argc, char *argv[])
 {
   int fd, i;
 
-  if(argc <= 1){
+  // parse options
+  for(i = 1; i < argc; i++){
+    if(argv[i][0] != '-')
+      break; 
+
+    if(strcmp(argv[i], "-l") == 0)
+      flag_l = 1;
+    else if(strcmp(argv[i], "-w") == 0)
+      flag_w = 1;
+    else if(strcmp(argv[i], "-c") == 0)
+      flag_c = 1;
+    else{
+      printf("wc: invalid option %s\n", argv[i]);
+      exit(1);
+    }
+  }
+
+  // no option -> print all
+  if(!flag_l && !flag_w && !flag_c){
+    flag_l = flag_w = flag_c = 1;
+  }
+
+  // stdin
+  if(i == argc){
     wc(0, "");
     exit(0);
   }
 
-  for(i = 1; i < argc; i++){
+  for(; i < argc; i++){
     if((fd = open(argv[i], O_RDONLY)) < 0){
       printf("wc: cannot open %s\n", argv[i]);
       exit(1);
